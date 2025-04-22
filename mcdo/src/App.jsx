48776fb9e8297db7db63f4333b12ccbbd8154894
AdieDev0 <devutella@gmail.com>
2025-04-22 23:06:22 +0800

    Refactor App component to include Navbar and Footer within Router structure

--- mcdo/src/App.jsx
+++ mcdo/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Routes, Router, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Navbar />
+      <Router>
+        <Route path="/footer" component={Footer} />
+      </Router>
+      <Footer />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
