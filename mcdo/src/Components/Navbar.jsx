6af4ddbbf4ca9641bee2b8c9f326b1ca3583a4e5
AdieDev0 <devutella@gmail.com>
2025-04-24 22:36:45 +0800

    Enhance Navbar component styling for improved user experience

--- mcdo/src/Components/Navbar.jsx
+++ mcdo/src/Components/Navbar.jsx
@@ -3,16 +3,28 @@ import { RiMenu3Fill } from "react-icons/ri";
 import Mcdo from "../assets/McD-png.png";
 const Navbar = () => {
   return (
-    <div>
+    <div className="px-72 py-5">
       <div className="text-center flex items-center justify-between">
         <img src={Mcdo} alt="" className="w-20" />
-        <button>Home</button>
-        <button>About Us</button>
-        <button>Family Activities</button>
-        <button>McDelivery</button>
-        <button>Careers</button>
-        <button>Opportunities</button>
-        <RiMenu3Fill />
+        <button className="text-red-600 hover:text-black duration-200 text-lg font-bold cursor-pointer">
+          Home
+        </button>
+        <button className="text-red-600 hover:text-black duration-200 text-lg font-bold cursor-pointer">
+          About Us
+        </button>
+        <button className="text-red-600 hover:text-black duration-200 text-lg font-bold cursor-pointer">
+          Family Activities
+        </button>
+        <button className="text-red-600 hover:text-black duration-200 text-lg font-bold cursor-pointer">
+          McDelivery
+        </button>
+        <button className="text-red-600 hover:text-black duration-200 text-lg font-bold cursor-pointer">
+          Careers
+        </button>
+        <button className="text-red-600 hover:text-black duration-200 text-lg font-bold cursor-pointer">
+          Opportunities
+        </button>
+        <RiMenu3Fill className="text-red-600 hover:text-black duration-200 size-8 cursor-pointer" />
       </div>
     </div>
   );
