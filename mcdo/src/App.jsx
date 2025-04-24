f7aabb28ed19565f82978e1ebf4316ee21605881
AdieDev0 <devutella@gmail.com>
2025-04-24 22:25:20 +0800

    Refactor App component to include Home route and remove footer route

--- mcdo/src/App.jsx
+++ mcdo/src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-const App = () => { 
+import Home from "./Components/Home";
+const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/footer" component={Footer} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
